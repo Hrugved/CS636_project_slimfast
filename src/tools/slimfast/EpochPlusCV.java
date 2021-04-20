de15132c2238de78cbbe4058a42e1851383a5251
hrugved <whrugved@gmail.com>
2021-04-20 20:18:35 +0530

    Change hashmap to array

--- src/tools/slimfast/EpochPlusCV.java
+++ src/tools/slimfast/EpochPlusCV.java
@@ -4,16 +4,12 @@ import rr.state.ShadowVar;
 import tools.util.Epoch;
 import tools.util.VectorClock;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class EpochPlusCV extends EpochPair implements ShadowVar {
 
     public VectorClock RVC = new VectorClock(SlimFastTool.INIT_VECTOR_CLOCK_SIZE);
 
-
-    private HashMap<Integer, EpochPlusCV> next = new HashMap<>();
+    private EpochPlusCV[] next = new EpochPlusCV[SlimFastTool.INIT_VECTOR_CLOCK_SIZE];
 
     public EpochPlusCV(int/* epoch */ W) {
         this.W = W;
@@ -23,19 +19,33 @@ public class EpochPlusCV extends EpochPair implements ShadowVar {
     public EpochPlusCV(EpochPlusCV epcv) {
         this(epcv.W);
         RVC.copy(epcv.RVC);
+        ensureCapacity(RVC.size());
     }
 
     public EpochPlusCV getNextEpcv(int/* epoch */ threadEpoch) {
         int tid = Epoch.tid(threadEpoch);
-        EpochPlusCV epcv = next.get(tid);
+        ensureCapacity(tid+1);
+        EpochPlusCV epcv = next[tid];
         if (epcv == null) {
             epcv = new EpochPlusCV(this);
         }
         if (epcv.RVC.get(tid) != threadEpoch) {
             epcv.RVC.set(tid, threadEpoch);
-            next.put(tid, epcv);
+            next[tid] = epcv;
         }
         return epcv;
     }
+
+    final private void ensureCapacity(int len) {
+        int curLength = next.length;
+        if (curLength < len) {
+            EpochPlusCV[] b = new EpochPlusCV[len];
+            for (int i = 0; i < curLength; i++) {
+                b[i] = next[i];
+            }
+            next = b;
+        }
+    }
+
 
 }
